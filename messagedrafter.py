f33316004011bd1e8c6e456ba196aef4305258be
Yashasvaddi <yashas.vaddi12@gmail.com>
2025-04-08 20:55:15 +0530

    Picture feature added

--- messagedrafter.py
+++ messagedrafter.py
@@ -7,7 +7,7 @@ import threading as th
 def caption():
     load_dotenv()
     api_key = os.getenv("GOOGLE_API_KEY")
-
+    image_upload=st.file_uploader("Upload the image files",type=["jpg","jpeg","png"],accept_multiple_files=True)
     if not api_key:
         st.error("API Key not found. Please check your .env file.")
         st.stop()
@@ -19,17 +19,17 @@ def caption():
 
     if st.button("Try it?") and user_text:
         with st.spinner("Cooking up fresh captions"):
-            folder_path = r"images"
-            image_exts = (".png", ".jpg", ".jpeg", ".webp", ".bmp")
+            # folder_path = r"images"
+            # image_exts = (".png", ".jpg", ".jpeg", ".webp", ".bmp")
 
-            image_paths = sorted([
-                os.path.join(folder_path, f)
-                for f in os.listdir(folder_path)
-                if f.lower().endswith(image_exts)
-            ])
+            # image_paths = sorted([
+            #     os.path.join(folder_path, f)
+            #     for f in os.listdir(folder_path)
+            #     if f.lower().endswith(image_exts)
+            # ])
 
             answer = ''
-            for path in image_paths:
+            for path in image_upload:
                 img = Image.open(path)
                 response = model.generate_content([
                     f'Give me a good caption for this picture. Use {user_text} for help. Keep it descriptive (up to 5 lines). No aesthetic or abstract captions, just meaningful ones.',
